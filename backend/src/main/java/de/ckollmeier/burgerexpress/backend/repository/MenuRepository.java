212e2e3d5eff9af172779e7c33384164ff858424
Carsten Kollmeier <carsten@ckollmeier.de>
2025-05-28 21:41:33 +0200

    Add MenuRepository interface with method to fetch all menus ordered by position

---
+++ backend/src/main/java/de/ckollmeier/burgerexpress/backend/repository/MenuRepository.java
@@ -0,0 +1,12 @@
+package de.ckollmeier.burgerexpress.backend.repository;
+
+import de.ckollmeier.burgerexpress.backend.model.Menu;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MenuRepository extends MongoRepository<Menu, String> {
+    List<Menu> findAllByOrderByPositionAsc();
+}
